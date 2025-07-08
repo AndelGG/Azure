061689286deea62d0f0bdb2b70f4d9612b4fc871
player0Gg <playeronlina@gmail.com>
2025-07-08 23:42:04 +0500

    Auth

---
+++ backend/src/auth/models.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Column, Integer, String, JSON
+from db import Base
+
+class User(Base):
+    __tablename__="user"
+
+    id=Column(Integer, primary_key=True, autoincrement=True)
+    email=Column(String(100))
+    username=Column(String(50))
+    password=Column(String(225))
+    film_subs=Column(JSON)
+    num_watch=Column(Integer)
+    view_movie=Column(JSON)
+    token=Column(String(225))
